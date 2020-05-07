cf7622e7e11daa334d579bdac28c4f2ef195a502
Alvin Berthelot <alvin.berthelot@webyousoon.com>
2020-05-07 06:56:02 +0200

    test: Improve square$

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,6 +1,8 @@
-import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import { Observable } from "rxjs"
+import { map } from "rxjs/operators"
 
-export function square(v: Observable<number>): Observable<number> {
-  return v.pipe(map(v => v * v));
+export function square$(
+  v: Observable<number>
+): Observable<number> {
+  return v.pipe(map((v) => v * v))
 }
